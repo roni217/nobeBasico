6e61352de2ced918bf1d2b7d64be483a659f3f5c
roni <roni.217@gmail.com>
2020-05-29 16:28:59 -0300

    adicionando express e EJS

--- noticias.js
+++ noticias.js
@@ -4,13 +4,13 @@ var server = http.createServer(function(req, res){
     var categoria = req.url;
 
     if(categoria=='/tecnologia'){
-        res.end("<html><body>Notícias de Tecnologia</html></body>")
+        res.end("<html><body>Notícias de Tecnologia</body></html>")
     } else if(categoria=='/moda'){
-        res.end("<html><body>Notícias de Moda</html></body>")
+        res.end("<html><body>Notícias de Moda</body></html>")
     } else if(categoria=='/beleza'){
-        res.end("<html><body>Notícias de beleza</html></body>")
+        res.end("<html><body>Notícias de Beleza</body></html>")
     } else {
-        res.end("<html><body>Portal de Notícias</html></body>")
+        res.end("<html><body>Portal de Notícias</body></html>")
     }
 });
 
